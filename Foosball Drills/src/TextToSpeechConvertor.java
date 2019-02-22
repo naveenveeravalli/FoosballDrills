39fef8f03958d25f2bafdc0901189c98cb2ecea0
naveenveeravalli <44479519+naveenveeravalli@users.noreply.github.com>
2019-02-22 00:09:12 -0800

    Update TextToSpeechConvertor.java

--- Foosball Drills/src/TextToSpeechConvertor.java
+++ Foosball Drills/src/TextToSpeechConvertor.java
@@ -9,10 +9,12 @@ public class TextToSpeechConvertor {
 
     // Some available voices are (kevin, kevin16, alan)
     private static final String VOICE_NAME_KEVIN = "kevin16";
+    //    private static final String VOICE_NAME_KEVIN = "alan"; //uncomment if setting the System.setProperty to AlanVoiceDirectory in line 17
     private final Voice voice;
 
     public TextToSpeechConvertor() {
-
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+//        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_time_awb.AlanVoiceDirectory");
         VoiceManager vm = VoiceManager.getInstance();
         voice = vm.getVoice(VOICE_NAME_KEVIN);
         voice.allocate();
